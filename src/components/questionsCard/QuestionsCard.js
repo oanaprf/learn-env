8f097a6f32fa1fff21b1868d12bcc81ee541e3c6
Profir Oana <oannna1234@gmail.com>
2022-09-19 17:40:01 +0300

    displayed correct count on learn env

--- src/components/questionsCard/QuestionsCard.js
+++ src/components/questionsCard/QuestionsCard.js
@@ -7,7 +7,7 @@ import "./styles.css";
 import Answers from "./Answers";
 import AnswerStatus from "./AnswerStatus";
 import Buttons from "./Buttons";
-import { TEST_QUESTIONS_COUNT } from "../../constants";
+import { ALL_QUESTIONS_COUNT, TEST_QUESTIONS_COUNT } from "../../constants";
 
 const QuestionsCard = ({ originalData, status, setStatus, isTestEnv, data, setData }) => {
   const [currentQuestion, setCurrentQuestion] = useState();
@@ -16,6 +16,7 @@ const QuestionsCard = ({ originalData, status, setStatus, isTestEnv, data, setDa
   const { t } = useTranslation();
 
   const hasAnsweredCorrectly = isEqual(selectedAnswer, currentQuestion?.correct[0]);
+  const totalCount = isTestEnv ? TEST_QUESTIONS_COUNT : ALL_QUESTIONS_COUNT;
 
   useEffect(() => {
     setCurrentQuestion(data?.splice(Math.floor(Math.random() * data?.length), 1)?.[0]);
@@ -23,22 +24,20 @@ const QuestionsCard = ({ originalData, status, setStatus, isTestEnv, data, setDa
 
   return (
     <div className="questions-card-container">
-      {isTestEnv && (
-        <div className="answers-status">
-          <span>
-            <span className="label">{t("totalAnswered")}: </span>
-            {TEST_QUESTIONS_COUNT - +data?.length}/{TEST_QUESTIONS_COUNT}
+      <div className="answers-status">
+        <span>
+          <span className="label">{t("totalAnswered")}: </span>
+          {totalCount - +data?.length}/{totalCount}
+        </span>
+        <span>
+          <span className="correct-container">
+            <span className="label">{t("correct")}: </span>
+            <span className="correct-count">{answersStatus.correct}</span>
           </span>
-          <span>
-            <span className="correct-container">
-              <span className="label">{t("correct")}: </span>
-              <span className="correct-count">{answersStatus.correct}</span>
-            </span>
-            <span className="label">{t("incorrect")}: </span>
-            <span className="incorrect-count">{answersStatus.incorrect}</span>
-          </span>
-        </div>
-      )}
+          <span className="label">{t("incorrect")}: </span>
+          <span className="incorrect-count">{answersStatus.incorrect}</span>
+        </span>
+      </div>
       <Card title={currentQuestion?.question}>
         <Answers {...{ currentQuestion, status, selectedAnswer, setSelectedAnswer }} />
         <AnswerStatus {...{ status, hasAnsweredCorrectly }} />
